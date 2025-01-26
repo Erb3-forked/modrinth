f3d584d2198930bd9bd3022694dae95c3145ef6b
Erb3 <49862976+Erb3@users.noreply.github.com>
2025-01-26 21:13:33 +0100

    refactor(utils): move into src folder

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,8 +1,7 @@
+export * from './billing'
 export * from './highlight'
 export * from './parse'
 export * from './projects'
+export * from './types'
 export * from './users'
 export * from './utils'
-export * from './billing'
-
-export * from './types'
